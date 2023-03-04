971f7908ae000e8ca55eb15a8aa5e0b0a60d65c4
Finbarrs Oketunji <f@finbarrs.eu>
2023-03-04 01:38:25 +0000

    v1.0.1
    Signed-off-by: Finbarrs Oketunji <f@finbarrs.eu>

--- authentication.go
+++ authentication.go
@@ -5,15 +5,16 @@ import (
 	"net/http"
 )
 
-func Authentication(apiKey string, org string) error {
-	url := "https://api.openai.com/v1/organizations/" + org + "/environments"
+func Authentication(apiKey string, orgID string) error {
+	url := "https://api.openai.com/v1/models"
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return fmt.Errorf("error creating request: %v", err)
 	}
 
-	req.Header.Set("Authorization", "Bearer "+apiKey)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+	req.Header.Set("OpenAI-Organization", orgID)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -24,7 +25,7 @@ func Authentication(apiKey string, org string) error {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("Authentication failed with status code %d", resp.StatusCode)
+		return fmt.Errorf("authentication failed with status code %d", resp.StatusCode)
 	}
 
 	return nil
